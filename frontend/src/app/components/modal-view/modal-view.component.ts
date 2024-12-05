f4d81465898a7c6651b9ccbdc6e2b5711640c5c6
XavierPC <xavierchen24@gmail.com>
2024-12-05 17:42:19 +0800

    updated modal compoent

--- frontend/src/app/components/modal-view/modal-view.component.ts
+++ frontend/src/app/components/modal-view/modal-view.component.ts
@@ -6,47 +6,94 @@ import { MatCardModule } from '@angular/material/card';
 import { CanvasJSAngularChartsModule } from '@canvasjs/angular-charts';
 // Angular Chart Component
 import { AgCharts } from 'ag-charts-angular';
-import { AgChartOptions } from "ag-charts-enterprise";
-import "ag-charts-enterprise";
-
+import { AgChartOptions } from 'ag-charts-enterprise';
+import 'ag-charts-enterprise';
+import { environment } from '../../../../environments/environment';
+import { EnergyService } from '../../services/energy.service';
+import { CommonService } from '../../services/common.service';
+import moment from 'moment';
 
 @Component({
   selector: 'app-modal-view',
   standalone: true,
-  imports: [CommonModule,MatIconModule, MatCardModule, CanvasJSAngularChartsModule,AgCharts],
+  imports: [
+    CommonModule,
+    MatIconModule,
+    MatCardModule,
+    CanvasJSAngularChartsModule,
+    AgCharts,
+  ],
   templateUrl: './modal-view.component.html',
-  styleUrl: './modal-view.component.css'
+  styleUrl: './modal-view.component.css',
 })
 export class ModalViewComponent {
-  countryName: String = "test";
-  capacity: number = 1234;
+  params = {
+    lat: environment.countriesLatLong[0].lat,
+    long: environment.countriesLatLong[0].long,
+    startDate: this.commonService.formatDateToYYYYMMDD(),
+    endDate: this.commonService.formatDateToYYYYMMDD(),
+  };
+  countryName: String = environment.countriesLatLong[0].name;
+  capacity: number = environment.countriesLatLong[0].capacity_kw;
   inv: number = 12;
   scb: number = 12;
   irridiance: number = 345.1;
-  days_online: number = 12;
+  today = moment();
+  date_online_configured = moment(environment.countriesLatLong[0].date_online);
+  days_online: number = this.today.diff(this.date_online_configured, 'days');
   performance_ratio: number = 60;
   inverter_efficiency: number = 81;
-  location: String = "12N15E"
-  power_generation: number = 1000;
+  power_generation: number = 1135;
 
-  private _data?: Data[]; // Example type
-
-  lineChartOptions = {
+  lineChartOptions: any = {
     animationEnabled: true,
     theme: 'light2', // "light1", "dark1", "dark2"
     data: [
       {
         type: 'line',
-        dataPoints: [
-          { label: 'apple', y: 10 },
-          { label: 'orange', y: 15 },
-          { label: 'banana', y: 25 },
-          { label: 'mango', y: 30 },
-          { label: 'grape', y: 28 },
-        ],
+        dataPoints: [],
       },
     ],
   };
+
+  private _data?: Data[]; // Example type
+
+  ngOnInit(): void {
+    this.energyService.getEnergyData(this.params).then((response) => {
+      const dailyData = response.hourly.direct_radiation.map(
+        (irridiance: string, index: number) => ({
+          label: moment(response.hourly.time[index]).format('HH:mm'),
+          y: irridiance,
+        })
+      );
+      this.lineChartOptions = {
+        options: {
+          responsive: true,
+        },
+        color: 'teal',
+        backgroundColor: 'transparent',
+        axisX: {
+          titleFontColor: 'white', // White color for the X-axis title
+          labelFontColor: 'white', // White color for the X-axis labels
+          lineColor: 'white', // White color for X-axis line
+        },
+        axisY: {
+          titleFontColor: 'white', // White color for the Y-axis title
+          labelFontColor: 'white', // White color for the Y-axis labels
+          lineColor: 'white', // White color for Y-axis line
+        },
+        animationEnabled: true,
+        theme: 'light2', // "light1", "dark1", "dark2"
+        data: [
+          {
+            color: '#01F9C6',
+            type: 'area',
+            dataPoints: dailyData,
+          },
+        ],
+      };
+    });
+  }
 
   @Input()
   set data(value: Data[] | undefined) {
@@ -63,26 +110,32 @@ export class ModalViewComponent {
     this.closeDrawerEmitter.emit();
   }
 
+  //Start of Radial bar
   public options: AgChartOptions;
 
-  constructor() {
+  constructor(
+    public commonService: CommonService,
+    public energyService: EnergyService
+  ) {
+    const todayGeneration = energyService.getEnergyData(this.params);
     this.options = {
       data: [
         {
           quarter: ``,
-          services: 3.91,
-        }
+          services: this.power_generation,
+        },
       ],
-      width:100,
-      height:100,
+      width: 200,
+      height: 200,
+      background: { fill: 'rgb(0, 0, 0, 0)' },
       series: [
         {
-          type: "radial-bar",
-          radiusKey: "quarter",
-          angleKey: "services",
-          angleName: "Services",
+          type: 'radial-bar',
+          radiusKey: 'quarter',
+          angleKey: 'services',
         },
       ],
     } as AgChartOptions;
   }
+  //End of Radial Bar
 }
